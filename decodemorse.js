3e13fc95837719db1bea6493d4d48b513c6b7a6c
psjfoster <psjfoster@gmail.com>
2023-07-13 13:20:34 +0100

    Create decodemorse.js

---
+++ decodemorse.js
@@ -0,0 +1,12 @@
+decodeMorse = function(morseCode){
+    let morseWords = morseCode.split("   ");
+    
+    // for each morseWord index
+    morseWords.forEach()
+    // create a morseLetters array
+    
+    // return a decoded word to a solution array 
+    // join decoded words into a string (with spaces between!)
+  }
+
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
